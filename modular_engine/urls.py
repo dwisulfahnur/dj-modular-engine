efc8f528d075c6350d158bde9a3fc1f227086d08
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-03-15 00:38:46 +0700

    feat: update the urlpatterns for the modular app

--- modular_engine/urls.py
+++ modular_engine/urls.py
@@ -4,8 +4,8 @@ from modular_engine import views
 app_name = 'modular_engine'
 
 urlpatterns = [
-    path('module/', views.ModuleListView.as_view(), name='module_list'),
-    path('module/install/<str:module_id>/', views.install_module, name='install_module'),
-    path('module/uninstall/<str:module_id>/', views.uninstall_module, name='uninstall_module'),
-    path('module/upgrade/<str:module_id>/', views.upgrade_module, name='upgrade_module'),
+    path('', views.ModuleListView.as_view(), name='module_list'),
+    path('install/<str:module_id>/', views.install_module, name='install_module'),
+    path('uninstall/<str:module_id>/', views.uninstall_module, name='uninstall_module'),
+    path('upgrade/<str:module_id>/', views.upgrade_module, name='upgrade_module'),
 ] 
